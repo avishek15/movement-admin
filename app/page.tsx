efcf801a64b5f30c8de6ae55dfd2ed150e8cc197
avishek15 <avishek.alex15@gmail.com>
2024-09-12 17:13:25 +0800

    Basic API call with axios

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,20 @@
-// Import the Image component from Next.js
 import Navbar from "@/components/Navbar";
-import Image from "next/image";
-// import AuthService from "@/services/auth.service";
+import axiosInstance from "@/configs/axiosinstance";
 
 // Define the Home component
 export default async function Home() {
-  // const authService = AuthService.getInstance();
-  // const loggedUser = await authService.getAccount();
-  // console.log("AUTH SERVICE ON PAGE", loggedUser);
+  const response = await axiosInstance({
+    url: "http://localhost:3001/api/userDetails",
+    method: "get",
+  });
+  const { name } = response.data;
   return (
-    // Main container with flexbox layout, minimum height of the screen, centered content, and padding
     <main className="flex-col min-h-screen items-center justify-between p-24">
-      {/* <ToastContainer /> */}
       <Navbar />
 
       <div className="text-center mt-4">
-        {/* Heading with large font size, bold style, and black color */}
-        <h1 className="text-4xl font-bold text-white">Hello World</h1>
+        <h1 className="text-4xl font-bold text-white">Hello, </h1>
+        <p>{name}</p>
       </div>
     </main>
   );
