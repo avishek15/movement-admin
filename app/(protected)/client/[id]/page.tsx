326e1c26383c1cf76637295f70f88a28a3b5e750
Siddharth Agarwal <40302363+zweistein1326@users.noreply.github.com>
2024-09-27 16:18:14 +0800

    update sidebar, update my-clients & all-clients page designs,  add client/[id]/*

---
+++ app/(protected)/client/[id]/page.tsx
@@ -0,0 +1,79 @@
+// import { useRouter } from "next/router";
+
+import Image from "next/image";
+import Link from "next/link";
+
+const Page = ({ params }: { params: { id: string[] } }) => {
+  const user = {
+    uid: "1",
+    name: "John Doe",
+    email: "johndoe@gmail.com",
+    phone: "123-456-7890",
+    trainer_id: 123,
+    trainer_name: "Jane Smith",
+    image:
+      "https://movementfitnesshk.com/wp-content/uploads/2024/07/Gina-Lai.png",
+  };
+  //   const router = useRouter();
+  return (
+    <main className="flex flex-col min-h-screen items-center justify-between p-8 bg-white text-black w-full">
+      <div className="text-center mt-4 flex flex-col gap-8 w-full">
+        <div className="flex flex-row gap-2">
+          <Image
+            src={user.image}
+            height={60}
+            width={60}
+            alt={`${user.name} image`}
+            objectFit="cover"
+            className="rounded-full aspect-square object-cover"
+          />
+          <div className="flex flex-col items-start">
+            <h2 className="text-l font-bold">{user.name}</h2>
+            <p className="text-sm">{user.email}</p>
+            <p className="text-sm">{user.phone}</p>
+          </div>
+        </div>
+        <div className="flex flex-row justify-between items-stretch">
+          <LinkTile
+            href={`${user.uid}/goals`}
+            label="Personal Goals"
+            stat="Completed 9/15"
+          />
+          <LinkTile
+            href={`${user.uid}/body-mass-composition`}
+            label="Body Mass Composition"
+            stat="24 Entries"
+          />
+          <LinkTile
+            href="training-plan"
+            label="Training Plan"
+            stat="4 Phases / 18 Sessions"
+          />
+          <LinkTile href="workouts" label="Workout History" stat="76 Entries" />
+          <LinkTile href="workout-tracker" label="Track New Workout" stat="" />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+const LinkTile = ({
+  href,
+  label,
+  stat,
+}: {
+  href: string;
+  label: string;
+  stat: string;
+}) => {
+  return (
+    <Link href={href}>
+      <div className="flex flex-col items-center gap-2 p-4 bg-white border-2 rounded-xl border-primary">
+        <p className="font-bold">{label}</p>
+        <p className="text-sm">{stat}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default Page;
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,35 +1,91 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import Link from "next/link";
+import { LuUsers2 } from "react-icons/lu";
+import { PiUsersFour } from "react-icons/pi";
+import { FiSettings } from "react-icons/fi";
+import { IoLogOutOutline } from "react-icons/io5";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+
+const sidebarItems = [
+  {
+    label: "My Clients",
+    icon: LuUsers2,
+    href: "/my-clients",
+  },
+  {
+    label: "All Clients",
+    icon: PiUsersFour,
+    href: "/all-clients",
+  },
+  {
+    label: "Settings",
+    icon: FiSettings,
+    href: "/settings",
+  },
+  {
+    label: "Logout",
+    icon: IoLogOutOutline,
+    href: "/help",
+  },
+];
 
 const Sidebar = () => {
+  const pathname = usePathname();
+  useEffect(() => {}, [pathname]);
+
   return (
-    <aside className="fixed top-0 left-0 w-64 h-full bg-green-800 text-white shadow-lg z-40 pt-16">
-      <div className="p-4">
-        <h2 className="text-xl font-bold mb-6 text-center">Menu</h2>
+    <aside className="left-0 w-64 h-screen bg-primary text-white shadow-lg z-40 pt-16">
+      <div className="px-4 flex flex-col gap-4">
+        <TrainerInfo />
         <nav className="space-y-4">
-          <Link href="/my-clients">
-            <div className="flex items-center p-2 rounded-md hover:bg-green-700 cursor-pointer">
-              <span className="ml-2">My Clients</span>
-            </div>
-          </Link>
-          <Link href="/all-clients">
-            <div className="flex items-center p-2 rounded-md hover:bg-green-700 cursor-pointer">
-              <span className="ml-2">All Clients</span>
-            </div>
-          </Link>
-          <Link href="/settings">
-            <div className="flex items-center p-2 rounded-md hover:bg-green-700 cursor-pointer">
-              <span className="ml-2">Settings</span>
-            </div>
-          </Link>
-          <Link href="/help">
-            <div className="flex items-center p-2 rounded-md hover:bg-green-700 cursor-pointer">
-              <span className="ml-2">Help</span>
-            </div>
-          </Link>
+          {sidebarItems.map((item) => {
+            var isItemActive = pathname === item.href;
+            var className = isItemActive
+              ? "bg-white text-primary"
+              : "text-white";
+            return (
+              <Link href={item.href} key={item.label}>
+                <div
+                  className={`flex items-center p-3 rounded-md cursor-pointer ${className}`}
+                >
+                  {item.icon()}
+                  <span className="ml-2">{item.label}</span>
+                </div>
+              </Link>
+            );
+          })}
         </nav>
       </div>
     </aside>
+  );
+};
+
+const TrainerInfo = () => {
+  const trainer = {
+    name: "Kenny C",
+    image:
+      "https://movementfitnesshk.com/wp-content/uploads/2024/01/Kenny-Cheung-web600x800_Movement-Fitness.jpg",
+    description: "Personal Trainer | Body Transformation",
+  };
+  return (
+    <div className="flex flex-col items-center gap-1">
+      <Image
+        src={trainer.image}
+        className="aspect-square object-cover rounded-full"
+        alt="Kenny C"
+        unoptimized
+        width={80}
+        height={80}
+      />
+      <div className="flex flex-col items-center gap-0.5">
+        <h2>{trainer.name}</h2>
+        <span className="text-center text-gray-300 text-sm">
+          {trainer.description}
+        </span>
+      </div>
+    </div>
   );
 };
 
